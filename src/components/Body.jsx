06d431cefa9b9bdffeeb2cf1cd79bf744b75d7fb
Gulshan Kumar <gulshankumar725044@gmail.com>
2025-11-25 23:26:23 +0530

    redirect to login if not auth

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { BASE_URL } from '../utils/constant'
 import Footer from './Footer'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { addUser } from '../utils/userSlice'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser =async () =>{
+    if (userData) return;
+    try{
+      const res = await axios.get(BASE_URL + "/profile", {
+      withCredentials: true,
+    });
+    dispatch(addUser(res.data));
+  } catch(err){
+    if (err.status === 401){
+      navigate("/login");
+    }
+    console.log(err);
+  }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <div>
         <NavBar/>
